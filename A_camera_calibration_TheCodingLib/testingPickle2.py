6a9c5a9b9c58926a405a8d887823c90365b80178
Mattpetr <matheo.petring@gmx.de>
2022-02-22 10:31:12 +0100

    Initial commit

---
+++ A_camera_calibration_TheCodingLib/testingPickle2.py
@@ -0,0 +1,15 @@
+import cv2
+import pickle
+
+calib_result_pickle = pickle.load(open("camera_calib_pickle.p", "rb" ))
+mtx = calib_result_pickle["mtx"]
+optimal_camera_matrix = calib_result_pickle["optimal_camera_matrix"]
+dist = calib_result_pickle["dist"]
+
+distorted_image = cv2.imread("test36.png")
+"""Then, given an input image or video frame (i.e. distorted_image), we can undistort it using the following lines of code:"""
+undistorted_image = cv2.undistort(distorted_image, mtx, dist, None,
+                                    optimal_camera_matrix)
+undistorted_image=cv2.resize(undistorted_image,(0,0),fx=0.5,fy=0.5)
+cv2.imshow("di",undistorted_image)
+cv2.waitKey(0)
